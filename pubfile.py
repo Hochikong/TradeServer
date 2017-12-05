69b44e3b29b0854f55d629815849cd53b5d622ff
Hochikong <ckhoidea@hotmail.com>
2017-12-06 01:28:45 +0800

    1.完成下单撤单逻辑 2.剩余的问题：尚未解决check_orders的卖单检查 3.备注：计算价格时使用了round函数保留两位小数到分钱；不足5元手续费的话收足5元，暂时不 收过户费

--- pubfile.py
+++ pubfile.py
@@ -5,8 +5,12 @@
 # @File    : pubfile.py
 # @Software: PyCharm
 
+from flask import json
+import uuid
 import random
 import string
+import time
+import pymongo
 
 
 def generate_random_str(length):
@@ -54,7 +58,7 @@ def generate_and_write(raw_param, trader_doc):
     :return:
     """
     total = raw_param.split("-m ")[-1]
-    userid = generate_random_str(8)
+    userid = str(uuid.uuid1())
     usertoken = generate_random_str(50)
     trader_doc.insert_one({'user_id': userid, 'token': usertoken, 'total': total, 'balance': total})
     print('User ID: ', userid, '\n')
@@ -72,3 +76,116 @@ def helper_print():
     print(' -a  查询所有用户信息', '\n')
     print('exit: 退出工具', '\n')
     print('目前所有可用参数均强制使用')
+
+
+def json_to_dict(rawdata):
+    """
+    把request中的json转换为字典
+    :param rawdata: request.data
+    :return: python dict
+    """
+    jsonstr = rawdata
+    jsondict = json.loads(jsonstr, encoding='utf-8')
+    return jsondict
+
+
+def token_certify(document, header):
+    """
+    根据传入的headers找到trade_token值并在数据库中查找对应的ID，若存在则返回用户数据，否则返回错误信息
+    :param document: traders文档
+    :param header: request.headers
+    :return: python dict
+    """
+    query = [x for x in list(document.find()) if header['trade_token'] in list(x.values())]
+    if len(query) > 0:
+        return query[0]
+    else:
+        return {'status': 'Error', 'msg': 'No such user'}
+
+
+def check_orders(jdict, authinfo, taxR, feeR):   # 12.05卖出逻辑尚未完成
+    """
+    从原始json转化的dict中获取指定的值，防止输入不允许的内容
+    :param jdict: 交易请求中的jdict
+    :param authinfo: authentication information
+    :param taxR: 印花税率，int类型
+    :param feeR: 手续费率，int类型，不足5元算5元
+    :return: python dict
+    """
+    try:
+        order = {'code': jdict['code'], 'name': jdict['name']}
+
+        # 检查数量是否合规
+        if int(jdict['amount']) % 100 == 0:
+            order['amount'] = jdict['amount']
+        else:
+            return {'status': 'Error', 'msg': 'Amount should be multiple of 100'}
+
+        # 计算费用
+        if jdict['ops'] == 'bid':
+            ordertotal = round(int(order['amount']) * float(jdict['price']), 2)
+            ordertax = round(ordertotal*taxR, 2)  # 输入的税率和手续费率默认为float型,使用round截断小数
+            orderfee = round(ordertotal*feeR, 2)
+            if orderfee < 5:
+                orderfee = 5
+            if ordertotal+ordertax+orderfee > float(authinfo['balance']):  # balance在traders表上为字符串
+                return {'status': 'Error', 'msg': "You don't have enough money"}
+            else:
+                order['price'] = jdict['price']
+                order['total'] = str(ordertotal)
+                order['tax'] = str(ordertax)
+                order['fee'] = str(orderfee)
+                order['cost'] = str(round(ordertax+orderfee, 2))
+                order['order_time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                order['user_id'] = authinfo['user_id']
+                order['ops'] = jdict['ops']
+                order['order_id'] = generate_random_str(10)  # 根据ID才能撤单
+                return order
+
+        elif jdict['ops'] == 'offer':
+            ordertotal = round(int(order['amount']) * float(jdict['price']), 2)
+            ordertax = round(ordertotal * taxR, 2)
+            orderfee = round(ordertotal * feeR, 2)
+
+            if orderfee < 5:
+                orderfee = 5
+            order['price'] = jdict['price']
+            order['total'] = str(ordertotal)
+            order['tax'] = str(ordertax)
+            order['fee'] = str(orderfee)
+            order['cost'] = str(round(ordertax+orderfee, 2))
+            order['order_time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            order['user_id'] = authinfo['user_id']
+            order['ops'] = jdict['ops']
+            order['order_id'] = generate_random_str(10)
+
+            return order
+        else:
+            return {'status': 'Error', 'msg': "Wrong ops"}
+
+    except KeyError:
+        return {'status': 'Error', 'msg': "Invalid input or wrong requests body"}
+
+
+def mongo_auth_assistant(address, port, username, passwd, database):
+    """
+    用于简化MongoDB认证
+    :param address: ...
+    :param port: ...
+    :param username: ...
+    :param passwd: ...
+    :param database: 用户所属数据库
+    :return: 连接对象
+    """
+    connection = pymongo.MongoClient(address, port)
+    if connection.admin.authenticate(username, passwd, mechanism='SCRAM-SHA-1', source=database):
+        pass
+    else:
+        raise Exception('Error configure on user or password! ')
+    return connection
+
+
+
+
+
+
--- tradeserver/tserver.py
+++ tradeserver/tserver.py
@@ -8,28 +8,34 @@
 from tradeserver import app
 from flask import render_template, request, json, jsonify, session, abort
 from configparser import ConfigParser
+from pubfile import json_to_dict, token_certify, check_orders, mongo_auth_assistant
 import pymongo
-
 
 # --------------------------------------
 # Load config.ini and read configuration
 CONFIG_FILE = 'config.ini'
 DB_SECTION = 'DB'
+TRADE_SECTION = 'Trade'
 cfg = ConfigParser()
 cfg.read(CONFIG_FILE)
 
 
 # ------------------
 # Connect to MongoDB
-connection = pymongo.MongoClient(cfg.get(DB_SECTION, 'address'), int(cfg.get(DB_SECTION, 'port')))
-if connection.admin.authenticate(
-    cfg.get(DB_SECTION, 'user'),
-    cfg.get(DB_SECTION, 'passwd'),
-    mechanism='SCRAM-SHA-1',
-    source=cfg.get(DB_SECTION, 'database')):
-    pass
-else:
-    raise Exception('Error configure on user or password! ')
+connection = mongo_auth_assistant(cfg.get(DB_SECTION, 'address'),
+                                  int(cfg.get(DB_SECTION, 'port')),
+                                  cfg.get(DB_SECTION, 'user'),
+                                  cfg.get(DB_SECTION, 'passwd'),
+                                  cfg.get(DB_SECTION, 'database'))
+#connection = pymongo.MongoClient(cfg.get(DB_SECTION, 'address'), int(cfg.get(DB_SECTION, 'port')))
+#if connection.admin.authenticate(
+#    cfg.get(DB_SECTION, 'user'),
+#    cfg.get(DB_SECTION, 'passwd'),
+#    mechanism='SCRAM-SHA-1',
+#    source=cfg.get(DB_SECTION, 'database')):
+#    pass
+#else:
+#    raise Exception('Error configure on user or password! ')
 sysdatabase = connection[cfg.get(DB_SECTION, 'database')]
 collect_traders = sysdatabase['traders']
 collect_orders = sysdatabase['orders']
@@ -45,14 +51,47 @@ collect_orders = sysdatabase['orders']
 @app.route('/order', methods=['POST', 'GET'])
 def takeorder():
     """
-    Requests style:{}
+    下单请求:{code:str,name:str,ops:str,price:str,amount:str}
+    撤单请求:{ops:str,order_id:str}
+    需要把token放在请求头部trade_token键
     :return:
     """
+    # 处理下单、撤单的交易请求
     if request.method == 'POST':
-        jsonstr = request.data
-        jsondict = json.loads(jsonstr, encoding='utf-8')
-        print(jsondict)
-        return jsonify({'MESSAGE': 'hello'})
+        jdict = json_to_dict(request.data)  # trade message
+        authentication_result = token_certify(collect_traders, request.headers)  # 检查token
+        if 'Error' in list(authentication_result.values()):
+            return jsonify(authentication_result)
+        else:
+            # 撤单逻辑
+            if 'order_id' in list(jdict.keys()) and jdict['ops'] == 'cancel':
+                delete_obj = collect_orders.delete_one({'order_id': jdict['order_id']})
+                if delete_obj.deleted_count > 0:
+                    return jsonify({'status': 'Done', 'msg': 'Order cancel'})
+                else:
+                    return jsonify({'status': 'Error', 'msg': 'No such order'})
+            # 买入或者卖出的撤单
+            else:
+                # 根据ops进行操作，买入、卖出
+                # 先检查是否能进入撮合系统
+                check_result = check_orders(jdict, authentication_result,
+                                            float(cfg.get(TRADE_SECTION, 'taxrate')),
+                                            float(cfg.get(TRADE_SECTION, 'feerate')))
+                if 'Error' in list(check_result.values()):
+                    return jsonify(check_result)
+                else:
+                    collect_orders.insert_one(check_result)
+                    return jsonify({'status': 'Done', 'msg': {'order_id': check_result['order_id']}})
+    # 查询订单
+    if request.method == 'GET':
+        authentication_result = token_certify(collect_traders, request.headers)
+        if 'Error' in list(authentication_result.values()):
+            return jsonify(authentication_result)
+        else:
+            order_belong_to_user = list(collect_orders.find({'user_id': authentication_result['user_id']}))
+            for order in order_belong_to_user:
+                order.pop('_id')
+            return jsonify({'status': 'Done', 'msg': {'remain_orders': order_belong_to_user}})  # 返回剩余订单
 
 
 if __name__ == '__main__':
