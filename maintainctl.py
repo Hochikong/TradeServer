1ea586a0f886bee6cd255e53022d3ebb9e08b532
Hochikong <ckhoidea@hotmail.com>
2017-12-20 03:33:44 +0800

    增加了一个用于修改数据库撮合信号的功能

--- maintainctl.py
+++ maintainctl.py
@@ -6,7 +6,7 @@
 # @Software: PyCharm
 
 from configparser import ConfigParser
-from stockclib.omServ import modify_print, generate_and_write, helper_print
+from stockclib.omServ import modify_print, generate_and_write, helper_print, update_signal
 import pymongo
 import re
 
@@ -23,6 +23,8 @@ generateuser = re.compile("gen -m \d{1,8}")
 checkusers = re.compile("check -a")
 exitctl = re.compile("exit")
 helper = re.compile("help")
+signal_run = re.compile("signal -r")
+signal_halt = re.compile("signal -h")
 # deleteuser = re.compile("delete -id")
 
 if __name__ == "__main__":
@@ -37,19 +39,33 @@ if __name__ == "__main__":
         raise Exception('Error configure on user or password! ')
     sysdatabase = connection[cfg.get(DB_SECTION, 'database')]
     collect_traders = sysdatabase['traders']
+    collect_oms = sysdatabase['ordermatch_service']
 
     while True:
         cmd = input('What to do? ')
         if generateuser.match(cmd):
             generate_and_write(cmd, collect_traders)
+            print('\n')
         elif checkusers.match(cmd):
             rawdata = list(collect_traders.find())
             modify_print(rawdata)
+            print('\n')
         elif exitctl.match(cmd):
+            print('Exiting...', '\n')
             break
         elif helper.match(cmd):
             helper_print()
+            print('\n')
+        elif signal_run.match(cmd):
+            update_signal(collect_oms, 'run')
+            print('Order matching service running')
+            print('\n')
+        elif signal_halt.match(cmd):
+            update_signal(collect_oms, 'halt')
+            print('Order matching service stop')
+            print('\n')
         else:
             print('Wrong command!')
+            print('\n')
 
 
