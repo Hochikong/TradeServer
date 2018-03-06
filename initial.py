9ea05396aabb4cf0c2876df06ec466fb16ccbb09
CKHo <1097225749@qq.com>
2018-03-06 15:49:34 +0800

    增加功能

---
+++ initial.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/6 15:47
+# @Author  : CKHo
+# @Email   : ckhoidea@hotmail.com
+# @File    : initial.py
+# @Software: PyCharm
+
+from stockclib.omServ import mongo_auth_assistant
+from configparser import ConfigParser
+
+CONFIG_FILE = 'config.ini'
+DB_SECTION = 'DB'
+COLL_SECTION = 'Collections'
+cfg = ConfigParser()
+cfg.read(CONFIG_FILE)
+
+db = mongo_auth_assistant(cfg.get(DB_SECTION, 'address'),
+                          int(cfg.get(DB_SECTION, 'port')),
+                          cfg.get(DB_SECTION, 'user'),
+                          cfg.get(DB_SECTION, 'passwd'),
+                          cfg.get(DB_SECTION, 'database'))[cfg.get(DB_SECTION, 'database')]
+
+if __name__ == "__main__":
+    db[cfg.get(DB_SECTION, 'ordermatch_service_coll')].insert_one({'status': 'run'})
